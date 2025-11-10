5552a7497d1a24a23578d590873171c2715c326c
vvilll <156856594+vvilll@users.noreply.github.com>
2025-11-10 00:46:06 +0000

    data gathering complete

--- stock.py
+++ stock.py
@@ -8,10 +8,16 @@ from bs4 import BeautifulSoup
 
 def scraper():
     URL = "https://finance.yahoo.com/markets/world-indices/"
-    page = requests.get(URL)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    page = requests.get(URL, headers=headers)
     #print(results.prettify())
     soup = BeautifulSoup(page.content, "html.parser")
-
+    results = soup.find(id="svelte")
+    stockSymbols = results.find_all("span", class_="symbol yf-90gdtp")
+    stockNames = results.find_all("div", class_="leftAlignHeader companyName yf-362rys enableMaxWidth")
+    stockPrices = results.find_all("fin-streamer")
+    for i in range(len(stockSymbols)):
+        print(stockSymbols[i].text.strip() + "(" + stockNames[i].text.strip() + "): " +  stockPrices[i*6].text.strip())
 
 
 if __name__ == "__main__":

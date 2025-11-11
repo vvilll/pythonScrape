0fc55366e4825ae63b039411504de2c9d6e3e14d
vvilll <156856594+vvilll@users.noreply.github.com>
2025-11-11 03:08:26 +0000

    untested: gives user price of stock symbol

--- stock.py
+++ stock.py
@@ -5,19 +5,48 @@
 
 import requests
 from bs4 import BeautifulSoup
+import os
+import sys
 
 def scraper():
     URL = "https://finance.yahoo.com/markets/world-indices/"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     page = requests.get(URL, headers=headers)
-    #print(results.prettify())
+    # #print(results.prettify())
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find(id="svelte")
     stockSymbols = results.find_all("span", class_="symbol yf-90gdtp")
     stockNames = results.find_all("div", class_="leftAlignHeader companyName yf-362rys enableMaxWidth")
     stockPrices = results.find_all("fin-streamer")
-    for i in range(len(stockSymbols)):
-        print(stockSymbols[i].text.strip() + "(" + (stockNames[i].text.strip())[1:] + "): " +  stockPrices[i*6].text.strip())
+    if os.path.exists('.finance'):
+        pass
+    else:
+        os.makedirs('.finance')
+
+    for arg in (sys.argv[1:]):
+        arg = arg.upper()
+        for i in range(len(stockSymbols)):
+            usrPrice = -1
+            if arg == (stockSymbols[i].text.strip())[1:]:
+                usrPrice = stockPrices[i*6].text.strip()
+                print(stockNames[i].text.strip() + '\n' + 'Current price is $' + stockPrices[i*6].text.strip() + '\n')
+            for file in os.listdir('.finance'):
+                if file.startswith(arg):
+                    stock = open(file, 'r')
+                    print('The old price was$' + stock.readline().strip() + '\n')
+
+    
+
+        # print(stockNames[i].text.strip() + "(" + (stockSymbols[i].text.strip())[1:] + "): " +  stockPrices[i*6].text.strip())
+#need to print stock name
+#then print price from webData
+#then check finance folder for the stock
+    #if the stock is in the folder print old stock price
+    #then print difference between old price and new
+    #then delete old file
+#create file with stock price name being: <stockSymbol>_<yyyy-mm-dd>_<hh:mm:ss>
+
+
 
 
 if __name__ == "__main__":
